c7c5c27792ed4bf8032958c15a72e6d6be0d4844
naveed jamali <naveedjamali@outlook.com>
2021-06-26 17:28:53 +0500

    updated

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,53 +1,45 @@
-#import "AppDelegate.h"
-#import "GeneratedPluginRegistrant.h"
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
 #import <Flutter/Flutter.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+  FlutterMethodChannel* batteryChannel = [FlutterMethodChannel
+                                          methodChannelWithName:@"course.flutter.dev/battery"
+                                          binaryMessenger:controller];
+  
+  __weak typeof(self) weakSelf = self;
+  [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+    if ([@"getBatteryLevel" isEqualToString:call.method]) {
+      int batteryLevel = [weakSelf getBatteryLevel];
 
-    FlutterViewController* controller = (FlutterViewController*) self.window.rootViewController;
-    FlutterMethodChannel* batteryChannel = [FlutterMethodChannel methodChannelWithName:@"course.flutter.dev/battery" binaryMessenger:controller];
-
-
-    __weak typeof(self) weakSelf = self;
-    [batteryChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result){
-      if([@"getBatteryLevel" isEqualToString:call.method]){
-        int batteryLevel = [weakSelf getBatteryLevel];
-
-        if(batteryLevel == -1){
-          result([FlutterError errorWithCode:@"UNAVAILABLE"
-          message:@"Battery info not available"
-
-          details:nil
-          ]);
-
-          
-        }
-        else{
-          result(@(batteryLevel));
-        }
+      if (batteryLevel == -1) {
+        result([FlutterError errorWithCode:@"UNAVAILABLE"
+                              message:@"Battery info not available."
+                              details:nil]);
+      } else {
+        result(@(batteryLevel));
       }
-      else{
-        result(FlutterMethodNotImplemented);
-      }
-    }];
-
+    } else {
+      result(FlutterMethodNotImplemented);
+    }
+  }];
 
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (int) getBatteryLevel {
+- (int)getBatteryLevel {
   UIDevice* device = UIDevice.currentDevice;
   device.batteryMonitoringEnabled = YES;
-  if(device.batteryState == UIDeviceBatteryStateUnknown){
+  if (device.batteryState == UIDeviceBatteryStateUnknown) {
     return -1;
-
-  }else{
-     return (int) (device.batterLevel * 100 );
+  } else {
+    return (int)(device.batteryLevel * 100);
   }
 }
 
